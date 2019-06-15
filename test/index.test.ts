4cc0198cf1b9f7dbde6be65e5ce5efb9edc53987
Ben James <benhjames@sky.com>
2019-06-15 14:58:41 +0100

    Convert module to TypeScript
    See [#8](https://github.com/helmetjs/crossdomain/pull/8).
    
    Closes #7.

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,71 +1,74 @@
-const crossdomain = require('..')
+import connect from 'connect';
+import request from 'supertest';
+import { IncomingMessage, ServerResponse } from 'http';
 
-const assert = require('assert')
-const connect = require('connect')
-const request = require('supertest')
+import crossdomain = require('..')
 
-describe('crossdomain', function () {
-  function app (middleware) {
-    const result = connect()
-    result.use(middleware)
-    result.use((req, res) => { res.end('Hello world') })
-    return result
+describe('crossdomain', () => {
+  function app (middleware: ReturnType<typeof crossdomain>) {
+    const result = connect();
+    result.use(middleware);
+    result.use((_req: IncomingMessage, res: ServerResponse) => {
+      res.end('Hello world');
+    });
+    return result;
   }
 
   it('sets X-Permitted-Cross-Domain-Policies: none when called with no arguments', () => {
     return request(app(crossdomain()))
       .get('/')
       .expect('X-Permitted-Cross-Domain-Policies', 'none')
-      .expect('Hello world')
-  })
+      .expect('Hello world');
+  });
 
   it('sets X-Permitted-Cross-Domain-Policies: none when called with an empty object', () => {
     return request(app(crossdomain({})))
       .get('/')
       .expect('X-Permitted-Cross-Domain-Policies', 'none')
-      .expect('Hello world')
-  })
+      .expect('Hello world');
+  });
 
   it('can explicitly set the policy to "none"', () => {
     return request(app(crossdomain({ permittedPolicies: 'none' })))
       .get('/')
       .expect('X-Permitted-Cross-Domain-Policies', 'none')
-      .expect('Hello world')
-  })
+      .expect('Hello world');
+  });
 
   it('can set the policy to "master-only"', () => {
     return request(app(crossdomain({ permittedPolicies: 'master-only' })))
       .get('/')
       .expect('X-Permitted-Cross-Domain-Policies', 'master-only')
-      .expect('Hello world')
-  })
+      .expect('Hello world');
+  });
 
   it('can set the policy to "by-content-type"', () => {
     return request(app(crossdomain({ permittedPolicies: 'by-content-type' })))
       .get('/')
       .expect('X-Permitted-Cross-Domain-Policies', 'by-content-type')
-      .expect('Hello world')
-  })
+      .expect('Hello world');
+  });
 
   it('can set the policy to "all"', () => {
     return request(app(crossdomain({ permittedPolicies: 'all' })))
       .get('/')
       .expect('X-Permitted-Cross-Domain-Policies', 'all')
-      .expect('Hello world')
-  })
+      .expect('Hello world');
+  });
 
   it('cannot set the policy to "by-ftp-filename"', () => {
-    assert.throws(() => { crossdomain({ permittedPolicies: 'by-ftp-filename' }) })
-  })
+    expect(() => { crossdomain({ permittedPolicies: 'by-ftp-filename' }); }).toThrow();
+  });
 
-  it('cannot set the policy to invalid values', function () {
-    assert.throws(() => { crossdomain({ permittedPolicies: '' }) })
-    assert.throws(() => { crossdomain({ permittedPolicies: null }) })
-    assert.throws(() => { crossdomain({ permittedPolicies: 'NONE' }) })
-  })
+  it('cannot set the policy to invalid values', () => {
+    expect(() => { crossdomain({ permittedPolicies: '' }); }).toThrow();
+    expect(() => { crossdomain({ permittedPolicies: 'NONE' }); }).toThrow();
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    expect(() => { crossdomain({ permittedPolicies: null } as any); }).toThrow();
+  });
 
-  it('names its function and middleware', function () {
-    assert.strictEqual(crossdomain.name, 'crossdomain')
-    assert.strictEqual(crossdomain().name, 'crossdomain')
-  })
-})
+  it('names its function and middleware', () => {
+    expect(crossdomain.name).toStrictEqual('crossdomain');
+    expect(crossdomain().name).toStrictEqual('crossdomain');
+  });
+});
